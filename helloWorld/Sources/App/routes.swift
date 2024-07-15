8f54f86847aca37394e35e1356f9c2c3693d6464
KS <kyusang.dev@gmail.com>
2024-07-15 11:23:49 +0900

    파라미터 전달 라우트 greet 추가

--- helloWorld/Sources/App/routes.swift
+++ helloWorld/Sources/App/routes.swift
@@ -4,8 +4,15 @@ func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
-
+    
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+    
+    app.get("greet", ":name") { req async -> String in
+        guard let guest = req.parameters.get("name") else {
+            return "Hi!"
+        }
+        return "Hi, \(guest), greetings! Thanks for visiting us."
     }
 }
