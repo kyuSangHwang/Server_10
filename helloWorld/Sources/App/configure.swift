8dddfaa2ea34cd874db97fa0f88bd0ee191f8b3f
KS <kyusang.dev@gmail.com>
2024-07-16 09:31:29 +0900

    logger 설정

--- helloWorld/Sources/App/configure.swift
+++ helloWorld/Sources/App/configure.swift
@@ -2,6 +2,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    app.logger.logLevel = .debug // 기본 로그 레벨을 .debug 로 설정
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
