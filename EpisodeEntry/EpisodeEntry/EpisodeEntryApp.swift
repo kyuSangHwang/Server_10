cb2c5ba67d7e3f6e67cc9bdfb8cde8c0f2918696
KS <kyusang.dev@gmail.com>
2024-07-18 09:46:19 +0900

    Figma보고 실습 진행

---
+++ EpisodeEntry/EpisodeEntry/EpisodeEntryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodeEntryApp.swift
+//  EpisodeEntry
+//
+//  Created by 황규상 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EpisodeEntryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
